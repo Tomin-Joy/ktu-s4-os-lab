94410459c1b5d2f47d5cd701a2006ea3e5138058
Tomin Joy <67215154+Tomin-Joy@users.noreply.github.com>
2022-05-24 23:18:48 +0530

    Create shell1.sh

---
+++ Experiment 2/shell1.sh
@@ -0,0 +1,15 @@
+echo "Enter your name";
+read name;
+echo "Enter your semester";
+read sem;
+echo "Enter your batch";
+read batch;
+echo "Enter your roll number";
+read rollno;
+echo "Enter your register number";
+read regno;
+echo "Name: $name"
+echo "Semester: $sem"
+echo "Batch: $batch"
+echo "Roll Number: $rollno"
+echo "Register Number: $regno"
